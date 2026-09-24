5d9b98b1d9e2929ec787d354d983e41a273e41cf
MohammadMahdijmt <mahdimmjamaati@gmail.com>
2026-09-24 14:10:47 +0330

    edit

--- shop-project-js/src/main.jsx
+++ shop-project-js/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
-import { AppdataProvider } from './Context/AppcartContext.jsx'
+import { AppdataProvider } from './Context/AppdataContext.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppdataProvider>
